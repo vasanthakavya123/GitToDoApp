d3623c3bc61e027b0b078b661522741e5884d034
vasanthakavya123 <67051761+vasanthakavya123@users.noreply.github.com>
2020-06-22 20:12:09 +0530

    Commit1

---
+++ .history/index_20200619221355.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const app = express();
+
+/*const dotenv = require("dotenv");
+dotenv.config();
+
+//models
+const TodoTask = require("./models/TodoTask"); */
+
+ app.use("/static",express.static("public"));
+
+ app.use(express.urlencoded({extended:true}));
+
+ app.set("view engine", "ejs");
+
+ 
+
+
+
+app.get('/',(req, res) => {
+    res.render('todo.ejs');
+    });
+
+    app.post('/',(req,res)=>
+    {console.log(req.body);
+
+    });
+
+   /* const mongoose = require("mongoose");
+//connection to db
+mongoose.set("useFindAndModify", false);
+mongoose.connect(process.env.DB_CONNECT, { useNewUrlParser: true }, () => {
+console.log("Connected to db!");
+}); */
+
+app.listen(3000, () => console.log("Server Up and running"));
---
+++ .history/index_20200619222151.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const app = express();
+
+const dotenv = require("dotenv");
+dotenv.config();
+
+//models
+const TodoTask = require("./models/TodoTask"); 
+
+ app.use("/static",express.static("public"));
+
+ app.use(express.urlencoded({extended:true}));
+
+ 
+
+    const mongoose = require("mongoose");
+//connection to db
+mongoose.set("useFindAndModify", false);
+mongoose.connect(process.env.DB_CONNECT, { useNewUrlParser: true }, () => {
+   
+console.log("Connected to db!");
+});
+
+
+app.listen(3000, () => console.log("Server Up and running"));
+app.set("view engine", "ejs");
+
+app.get('/',(req, res) => {
+    res.render('todo.ejs');
+    });
+
+    app.post('/',(req,res)=>
+    {console.log(req.body);
+
+    });
